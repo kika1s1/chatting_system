aff0bdebe26e8630875e506bb455e35ef93c9078
kika1s1 <tamiratkebede120@gmail.com>
2024-07-08 12:32:59 +0300

    database is connected

--- server/index.js
+++ server/index.js
@@ -1,18 +1,28 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-import cors from 'cors'
-dotenv.config({})
-const app = express()
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
 
-app.use(cors({
-    origin:process.env.FRONTEND_URL,
-    credentials:true
-}))
+import connectDB from "./config/connectDb.js";
+connectDB();
+dotenv.config();
+const app = express();
 
-const PORT = process.env.PORT || 5000
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 
+const PORT = process.env.PORT || 5000;
 
-app.listen(()=>{
-    console.log("server is running " + PORT)
-})
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "server is running at " + PORT,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
